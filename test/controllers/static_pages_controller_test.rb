7d9dd5c4ebeb588edbe6c0e4a30e59bad7d7b1ac
Miles Au <miles.d.au@gmail.com>
2018-09-09 14:01:01 -0700

    Finish static pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,14 +1,27 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+
+  def setup
+    @base_title = "Promo Posts - share promo content in seconds"
+  end
+
   test "should get home" do
-    get static_pages_home_url
+    get root_url
     assert_response :success
+    assert_select "title", "#{@base_title}"
   end
 
   test "should get help" do
     get static_pages_help_url
     assert_response :success
+    assert_select "title", "Help | #{@base_title}"
+  end
+
+  test "should get contact" do
+    get static_pages_contact_url
+    assert_response :success
+    assert_select "title", "Contact | #{@base_title}"
   end
 
 end
