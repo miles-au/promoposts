878585e5ab7f83a4e8e342dc3b59ac6578b2b526
Miles Au <miles.d.au@gmail.com>
2018-09-19 20:53:09 -0700

    Finish user edit, update, index, and destroy actions

--- Gemfile
+++ Gemfile
@@ -8,6 +8,9 @@ gem 'rails', '~> 5.2.1'
 gem 'bootstrap-sass'
 gem 'bcrypt'
 gem 'jquery-rails'
+gem 'faker'
+gem 'will_paginate'
+gem 'bootstrap-will_paginate'
 # Use Puma as the app server
 gem 'puma', '~> 3.11'
 # Use SCSS for stylesheets
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,7 +9,7 @@ class SessionsController < ApplicationController
       flash[:success] = "Welcome to Promo Posts, #{user.name}."
       log_in user
       params[:session][:remember_me] == '1' ? remember(user) : forget(user)
-      redirect_to root_path
+      redirect_back_or root_path
     else
       flash.now[:danger] = 'Invalid email/password combination'
       render 'new'
