941a88825c4dc618a1a4bcb5bec51b99fd7067f3
Miles Au <miles.d.au@gmail.com>
2018-09-22 11:11:59 -0700

    not activated user, password reset but

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -10,10 +10,18 @@ class PasswordResetsController < ApplicationController
   def create
     @user = User.find_by(email: params[:password_reset][:email].downcase)
     if @user
-      @user.create_reset_digest
-      @user.send_password_reset_email
-      flash[:info] = "Email sent with password reset instructions"
-      redirect_to root_url
+      if @user.activated
+	      @user.create_reset_digest
+	      @user.send_password_reset_email
+	      flash[:info] = "Email sent with password reset instructions"
+	      redirect_to root_url
+	  else
+	  	message  = "Account not activated. "
+        message += "Check your email for the activation link. "
+        message += "#{view_context.link_to('Click here to resend link', sessions_resend_activation_path(email: user.email))}".html_safe
+        flash[:warning] = message
+        redirect_to root_url
+      end
     else
       flash.now[:danger] = "Email address not found"
       render 'new'
