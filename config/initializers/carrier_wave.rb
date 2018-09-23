06dbaee715a44f2f92417de319a91913acb51944
Miles Au <miles.d.au@gmail.com>
2018-09-23 09:52:38 -0700

    Add user microposts

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -3,8 +3,8 @@ if Rails.env.production?
 	  config.fog_provider = 'fog/google'                        # required
 	  config.fog_credentials = {
 	    provider:                         'Google',
-	    google_storage_access_key_id:     'GOOGLE_ID',
-	    google_storage_secret_access_key: 'GOOGLE_SECRET'
+	    google_storage_access_key_id:     ENV['GOOGLE_ID'],
+	    google_storage_secret_access_key: ENV['GOOGLE_SECRET']
 	  }
 	  config.fog_directory = 'name_of_directory'
 	end
