8996a8998e69ab94153a649224d3c0d7828f25b5
Miles Au <miles.d.au@gmail.com>
2018-11-04 17:18:31 -0800

    updating database

--- db/migrate/20181101020610_update_user_columns_for_oauth.rb
+++ db/migrate/20181101020610_update_user_columns_for_oauth.rb
@@ -1,6 +1,5 @@
 class UpdateUserColumnsForOauth < ActiveRecord::Migration[5.2]
   def change
-  	remove_column :users, :fb_autoshare
   	remove_column :users, :oauth_expires_at
   	remove_column :users, :oauth_token
   	remove_column :users, :uid
