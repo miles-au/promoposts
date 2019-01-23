9866de5397164d5cf6168726e6f10e790ee4f964
Miles Au <miles.d.au@gmail.com>
2019-01-22 21:02:08 -0800

    notifications pg sql

--- app/models/user.rb
+++ app/models/user.rb
@@ -365,7 +365,7 @@ class User < ApplicationRecord
 
   def notifications_today
     if Rails.env.production?
-      notifications = Notification.where( "user_id ILIKE '?' AND created_at > '?'", self.id, 1.day.ago)
+      notifications = Notification.where( "user_id = '?' AND created_at > '?'", self.id, 1.day.ago)
     else
       notifications = Notification.where( "user_id = ? AND created_at > ?", self.id, 1.day.ago)
     end 
@@ -375,7 +375,7 @@ class User < ApplicationRecord
 
   def unread_notifications
     if Rails.env.production?
-      notifications = Notification.where( "user_id ILIKE '?' AND read ILIKE '?'", self.id, false)
+      notifications = Notification.where( "user_id = '?' AND read = ?", self.id, false)
     else
       notifications = Notification.where( "user_id = ? AND read = ?", self.id, false)
     end 
