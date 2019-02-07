906327d899919ae4be91687eca28394103541711
Miles Au <miles.d.au@gmail.com>
2019-02-07 13:10:47 -0800

    basic error pages

--- config/application.rb
+++ config/application.rb
@@ -22,6 +22,6 @@ module PromoPosts
     config.session_store :cookie_store, key: '_interslice_session'
     config.middleware.use ActionDispatch::Cookies # Required for all session management
     config.middleware.use ActionDispatch::Session::CookieStore, config.session_options
-
+    
   end
 end
