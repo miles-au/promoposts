7ac5e0409954ac1709eb19a401c94a64f21cdd57
Miles Au <miles.d.au@gmail.com>
2019-03-11 22:17:47 -0700

    revert rack attack

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -23,12 +23,8 @@ class Rack::Attack
   # Throttle all requests by IP (60rpm)
   #
   # Key: "rack::attack:#{Time.now.to_i/:period}:req/ip:#{req.ip}"
-=begin
+  
   throttle('req/ip', limit: 300, period: 5.minutes) do |req|
-    req.ip # unless req.path.start_with?('/assets')
-  end
-=end
-  throttle('req/ip', limit: 5, period: 60.seconds) do |req|
     req.ip # unless req.path.start_with?('/assets')
   end
 
