ac0cea60273d732dcfbefe2b0acb0a63baa5f4a8
Miles Au <miles.d.au@gmail.com>
2019-03-19 19:09:48 -0700

    rack attack facebook whitelist

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -75,17 +75,16 @@ class Rack::Attack
   #    ['']] # body
   # end
 
-=begin
   # Provided that trusted users use an HTTP request header named APIKey
   Rack::Attack.safelist("facebook webhooks") do |req|
     # Requests are allowed if the return value is truthy
-    req_bod = req.body.read
-    req_env = req.env
-    #if Koala::Facebook::RealtimeUpdates.new( :app_id => ENV['FACEBOOK_KEY'] , :secret => ENV['FACEBOOK_SECRET']).validate_update( req_bod , req_env )
-      #true
-    #end
+    request = req
+    req_bod = request.body.read
+    req_env = request.env
+    if Koala::Facebook::RealtimeUpdates.new( :app_id => ENV['FACEBOOK_KEY'] , :secret => ENV['FACEBOOK_SECRET']).validate_update( req_bod , req_env )
+      true
+    end
   end
-=end
 
   Rack::Attack.safelist("test environment") do |req|
     # Requests are allowed if the return value is truthy
