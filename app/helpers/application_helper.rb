9a868f2126d60229062a4f7e1954eb08b498362e
Miles Au <miles.d.au@gmail.com>
2019-07-01 22:01:21 -0700

    updated titles

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
 
   # Returns the full title on a per-page basis.
   def full_title(page_title = '')
-    base_title = "Promo Posts - News, Content, Ideas"
+    base_title = "Promo Posts - Social media for promo made easy."
     if page_title.empty?
       base_title
     else
