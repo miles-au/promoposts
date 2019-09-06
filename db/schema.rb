d9c13a322447866c6db52f466e3a578b6d4303f1
Miles Au <miles.d.au@gmail.com>
2019-09-06 02:37:10 -0400

    timestamps without timezone

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_06_062223) do
+ActiveRecord::Schema.define(version: 2019_09_06_063629) do
 
   create_table "accolades", force: :cascade do |t|
     t.integer "user_id"
