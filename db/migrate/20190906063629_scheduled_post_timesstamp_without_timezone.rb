49dd827a9d08ae9fd3e40fad0673f41aaab3daf7
Miles Au <miles.d.au@gmail.com>
2019-09-06 02:42:55 -0400

    pg timestamps

--- db/migrate/20190906063629_scheduled_post_timesstamp_without_timezone.rb
+++ db/migrate/20190906063629_scheduled_post_timesstamp_without_timezone.rb
@@ -1,5 +1,5 @@
 class ScheduledPostTimesstampWithoutTimezone < ActiveRecord::Migration[5.2]
   def change
-  	change_column :scheduled_posts, :post_time, 'timestamp with time zone'
+  	change_column :scheduled_posts, :post_time, 'timestamp without time zone'
   end
 end
