dfe0df0ccaa3b2ce19f856e1fc3660f99e577647
Miles Au <miles.d.au@gmail.com>
2019-09-11 03:26:24 -0400

    modal bug

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,7 +27,7 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
@@ -105,7 +105,6 @@ Rails.application.configure do
     :enable_starttls_auto => true
   }
   config.serve_static_assets = true
-  config.assets.compile = true
 
   config.action_mailer.default_url_options = {
     host: 'https://www.promoposts.net'
