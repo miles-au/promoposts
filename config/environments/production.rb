e95c558e112d13610feaf4d401f6623ab0bfd6a0
Miles Au <miles.d.au@gmail.com>
2019-09-12 17:07:14 -0400

    config.serve_static_files = true

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -104,7 +104,7 @@ Rails.application.configure do
     :domain         => 'heroku.com',
     :enable_starttls_auto => true
   }
-  config.serve_static_files = false
+  config.serve_static_files = true
 
   config.action_mailer.default_url_options = {
     host: 'https://www.promoposts.net'
