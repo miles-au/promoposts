789be4e1b9ceb489e93e75de994e1d2f88831d60
Miles Au <miles.d.au@gmail.com>
2019-09-25 19:09:24 -0400

    promodigi yield title

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,5 +9,14 @@ module ApplicationHelper
       "Promo Posts | " + page_title
     end
   end
+
+  def promodigi_title(page_title = '')
+    base_title = "Custom Promotional Items"
+    if page_title.empty?
+      base_title
+    else
+      page_title + " | Custom Promotional Items"
+    end
+  end
   
 end
